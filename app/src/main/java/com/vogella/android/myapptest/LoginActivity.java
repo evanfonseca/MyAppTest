8a53bf461cd5f970eecafe4df1e421a1509014f5
enfonseca <evanfonseca@gmail.com>
2016-03-17 13:38:37 -0100

    just some changes

--- app/src/main/java/com/vogella/android/myapptest/LoginActivity.java
+++ app/src/main/java/com/vogella/android/myapptest/LoginActivity.java
@@ -61,7 +61,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
-    //some changes
+    //some changess
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
